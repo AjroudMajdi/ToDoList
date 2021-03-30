d75089bdd6cbd2af1a256ca6e9f5abebfc115205
Ajroud <majdi.ajroudi@target-energysolutions.com>
2021-03-30 11:30:30 +0100

    :art: add local storage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 
+import React, { useEffect, useState } from "react";
 import { navigate, Router } from "@reach/router"
 import Login from './login'
 import Home from './home'
+import Logout from './logout'
+
 
 
 import './App.css';
 
+const LOGIN_STORAGE_KEY = 'login-todo' 
+const LOGIN_STORAGE_VALUE = 'logged'
 
 const App  = ()=> {
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem(LOGIN_STORAGE_KEY) === LOGIN_STORAGE_VALUE
+    if(!isLoggedIn) {
+     navigate('/')
+    }
+   }, [])
+ 
   return (
     <div className="App">
     <Router>
-          <Login path="/"  />
+          <Login path="/" loginStorageKey={LOGIN_STORAGE_KEY} loginStorageValue={LOGIN_STORAGE_VALUE} />
           <Home path="/home" />
+          <Logout path="/logout" loginStorageKey={LOGIN_STORAGE_KEY} />
+
+
       </Router>
 
     </div>
--- src/home/home.js
+++ src/home/home.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TextField, Button } from "react-md";
+import { Link } from "@reach/router";
 
 import "./style.scss";
 
-const Home = () => {
+const Home = ({ tasksStorageKey }) => {
   const [nom, setNom] = useState("");
   const [tasks, setTasks] = useState([]);
   const [description, setDescription] = useState("");
+  useEffect(() => {
+    const data = localStorage.getItem("tasksStorageKey");
+    setTasks(data ? JSON.parse(data) : []);
+  }, []);
 
   const addtasks = () => {
     setTasks(tasks.includes(nom) ? tasks : [...tasks, { nom, description }]);
     setNom("");
+    localStorage.setItem("tasksStorageKey", JSON.stringify(tasks));
   };
 
   const removetask = (nom) => {
@@ -19,36 +25,57 @@ const Home = () => {
 
   return (
     <>
-      <div className="invite-member-field">
+      <div className="top-bar">
+        <span className="logo">To Do List : </span>
+        <Link className="btn danger sm" to="/logout">
+          Deconnexion
+        </Link>
+      </div>
+
+      <div className="taches">
+        <div className="taches-tacheslist">
+          <div className="title">Liste des Taches</div>
+          {tasks.map((mb, index) => (
+            <div className="item" key={index}>
+              <div className="name">
+                {" "}
+                <b>{mb.nom} :</b>
+              </div>
+              <div className="description">
+                <div>{mb.description}</div>
+              </div>
+
+              <Button
+                className="button"
+                primary
+                flat
+                onClick={() => removetask(mb.nom)}
+              >
+                Supprimer
+              </Button>
+            </div>
+          ))}
+        </div>
         <TextField
           value={nom}
-          className="invite-member-textField"
-          label="nom de la tache "
+          className="taches-textField"
+          label="nom de la tache: "
           onChange={({ target }) => setNom(target.value)}
         />{" "}
         <TextField
           value={description}
-          className="invite-member-textField"
-          label="Description"
+          className="taches-textField"
+          label="Description: "
           onChange={({ target }) => setDescription(target.value)}
         />
-        <Button primary flat onClick={() => addtasks()}>
+        <Button
+          className="taches-addbtn"
+          primary
+          flat
+          onClick={() => addtasks()}
+        >
           Ajouter la tache
         </Button>
-      </div>
-      <div className="emailList">
-        {tasks.map((mb, index) => (
-          <div className="emailList-item" key={index}>
-            <div className="name"> {mb.nom}</div>
-            <div className="information">
-              <div>{mb.description}</div>
-            </div>
-
-            <Button icon onClick={() => removetask(mb.nom)}>
-              Supprimer
-            </Button>
-          </div>
-        ))}
       </div>
     </>
   );
